7edcf807d3ac79a36776f083f0b390bda1e5bc34
Aaweshdas <aaravdas208@gmail.com>
2024-07-14 17:20:21 +0545

    Code

---
+++ Subtraction.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, sum;
+    cout << "Please enter the number :" << endl;
+    cin >> a;
+    cout << "Please enter the number :" << endl;
+    cin >> b;
+
+    sum = a+b;
+   cout << "Sum of Two Numbers is"  << a << " and " << b << " is " << sum << endl;
+    return 0;
+}

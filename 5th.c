22aaf8fdad968f526900def32a68370db92b2eb4
Aaweshdas <aaravdas208@gmail.com>
2024-08-30 21:22:55 +0545

    Code

--- 5th.c
+++ 5th.c
@@ -2,15 +2,6 @@
 int mail()
 {
     int a=1,b=4,c;
-        c=a+b;
-        c=a-b;
-        c=a*b;
-        c=a/b;
-        c=a%b;
-        printf("a+b=%d\n",c);
-         printf("a-b=%d\n",c);
-    printf("a*b=%d\n",c);
-      printf("a/b=%d\n",c);
-         printf("a%b=%d\n",c);
+  
             return 0;
 }

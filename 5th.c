256bee4d4a2babd1f2332f654c7bb59c3909699a
Aaweshdas <aaravdas208@gmail.com>
2024-08-30 21:22:29 +0545

    Code

---
+++ 5th.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int mail()
+{
+    int a=1,b=4,c;
+        c=a+b;
+        c=a-b;
+        c=a*b;
+        c=a/b;
+        c=a%b;
+        printf("a+b=%d\n",c);
+         printf("a-b=%d\n",c);
+    printf("a*b=%d\n",c);
+      printf("a/b=%d\n",c);
+         printf("a%b=%d\n",c);
+            return 0;
+}
